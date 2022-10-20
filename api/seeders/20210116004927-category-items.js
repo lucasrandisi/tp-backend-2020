fc1a232cbf88bd0cffa8b20db7ccd7cfe60488d7
FernandezNatalia <natalia77@live.com>
2022-10-19 23:37:31 -0300

    update sedeers

--- api/seeders/20210116004927-category-items.js
+++ api/seeders/20210116004927-category-items.js
@@ -1,8 +1,42 @@
-
-
 module.exports = {
 	up: (queryInterface) => {
 		return queryInterface.bulkInsert('categories_items', [
+			{
+				categoryId: 2,
+				itemId: 1,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 9,
+				itemId: 1,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 2,
+				itemId: 2,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 9,
+				itemId: 2,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 4,
+				itemId: 3,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 1,
+				itemId: 4,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
 			{
 				categoryId: 2,
 				itemId: 5,
@@ -10,14 +44,44 @@ module.exports = {
 				updatedAt: new Date(),
 			},
 			{
-				categoryId: 7,
-				itemId: 1,
+				categoryId: 10,
+				itemId: 6,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 1,
+				itemId: 7,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 1,
+				itemId: 8,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 1,
+				itemId: 9,
 				createdAt: new Date(),
 				updatedAt: new Date(),
 			},
 			{
 				categoryId: 5,
 				itemId: 10,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 5,
+				itemId: 11,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+			{
+				categoryId: 5,
+				itemId: 12,
 				createdAt: new Date(),
 				updatedAt: new Date(),
 			},
